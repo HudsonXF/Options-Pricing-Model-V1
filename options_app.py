9e8c089cfdfa414e0aef9561b2a20a90a0cb8b74
HudsonXF <hudsonxf@gmail.com>
2025-08-24 17:31:37 -0300

    Update options_app.py

--- options_app.py
+++ options_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from np.stats import norm
+from scipy.stats import norm
 import plotly.graph_objects as go
 from numpy import log, sqrt, exp
 import matplotlib.pyplot as plt
